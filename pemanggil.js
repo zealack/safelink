4d45f1f0f7915d7cd44cfa346f45fa6543013a12
zealack <35425057+zealack@users.noreply.github.com>
2018-02-16 05:44:14 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/01/post1.html"}
-if(x=="2"){xxx="/2018/01/post2.html"}
-if(x=="3"){xxx="/2018/01/post3.html"}
-if(x=="4"){xxx="/2018/01/post4.html"}
-if(x=="5"){xxx="/2018/01/post5.html"}a_to_ve[i].href="http://www.upgan.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/02/4-pertanyaan-paling-penting-sebelum.html"}
+if(x=="2"){xxx="/2018/02/apakah-anda-berbohong-tentang-tujuan.html"}
+if(x=="3"){xxx="/2018/02/kentut-tujuh-manfaat-kesehatan-yang.html"}
+if(x=="4"){xxx="/2018/02/apakah-itu-baik-baik-saja-untuk.html"}
+if(x=="5"){xxx="/2018/02/5-langkah-sederhana-untuk-gaya-hidup.html"}a_to_ve[i].href="http://safelink.salakzzz.tk"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
